85775db3984c0867139bdde636acb6ac16c8526c
Alex McPhee <awmcphee@ualberta.ca>
2022-03-26 20:05:04 -0600

    Data entry

--- src/pages/maps.js
+++ src/pages/maps.js
@@ -7,9 +7,9 @@ import Seo from "../components/seo"
 const maps = () => (
   <Layout>
     <Seo title="Maps" />
-    <h1>Map Gallery</h1>
+    <h1>Election Maps</h1>
     <h2>Printed in Canada</h2>
-    <p>Can't wait for the next election? Now's the time to read up on past ones. Polling Canada is pleased to sell a variety of unique election maps designed by our trusty staff cartographer, <a href="https://www.awmcphee.ca" >Alex McPhee</a>.</p>
+    <p>Can't wait for the next election? Polling Canada is pleased to sell a variety of unique election maps designed by our trusty staff cartographer, <a href="https://www.awmcphee.ca" >Alex McPhee</a>.</p>
     <h2>Federal Elections</h2>
     <p>Even in the world's largest democracy, land doesn't vote. Ever wonder why federal campaigns mostly revolve around suburban Toronto? These innovative "cartograms" provide a detailed and unbiased look into every riding.</p>
     <h2>Provincial Elections</h2>
