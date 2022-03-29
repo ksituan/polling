5c272af822a477afa5c3c21cbf7d7bedd82907c4
Alex McPhee <awmcphee@ualberta.ca>
2022-03-29 10:19:09 -0600

    More territorial election handling

--- src/templates/SingleProvince.js
+++ src/templates/SingleProvince.js
@@ -8,12 +8,12 @@ export default function SingleProvince({pageContext, data}) {
     const {jurisdiction, jurisdictionName, beforeDate} = pageContext;
     const polls = data.allPollsJson.nodes;
     const election = data.electionsJson;
-    const pageTitle = `${jurisdictionName} ${jurisdiction.split("_")[0] !== "Canada" ? (jurisdiction !== "Yukon" ? "territorial" : "provincial") : ""} polling`;
+    const pageTitle = `${jurisdictionName} ${jurisdiction.split("_")[0] !== "Canada" ? (jurisdiction === "YT" ? "territorial" : "provincial") : ""} polling`;
     return(
         <Layout>
         <Seo title={pageTitle} />
             <h1>{pageTitle}</h1>
-            <p>This page lists every publicly accessible poll conducted by a reputable firm since the {new Date(election.date).getFullYear()} {jurisdiction.split("_")[0] === "Canada" ? "federal" : jurisdictionName + " " + jurisdiction !== "Yukon" ? "territorial" : "provincial"} election. Many polls are privately commissioned and go unreleased, but some are leaked after the fact. To learn more about our standards, check our <Link to="/faq">FAQ</Link>.</p>
+            <p>This page lists every publicly accessible poll conducted by a reputable firm since the {new Date(election.date).getFullYear()} {jurisdiction.split("_")[0] === "Canada" ? "federal" : jurisdictionName + " " + (jurisdiction === "YT" ? "territorial" : "provincial")} election. Many polls are privately commissioned and go unreleased, but some are leaked after the fact. To learn more about Polling Canada's standards, check the <Link to="/faq">FAQ</Link>.</p>
             <PollingContent polls={polls} jurisdiction={jurisdiction} name={jurisdictionName} election={election} endDate={new Date(beforeDate)}/>
         </Layout>
     )
