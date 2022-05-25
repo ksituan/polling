920644509ece9168fee2fca109d1e92a3e89bd6d
Alex McPhee <awmcphee@ualberta.ca>
2022-05-25 16:59:25 -0600

    Add navigation arrows on desktop

--- src/templates/SingleProvince.js
+++ src/templates/SingleProvince.js
@@ -4,17 +4,46 @@ import Layout from '../components/layout';
 import PollingContent from "../components/PollingContent";
 import Seo from "../components/seo"
 
+let provinceOrder = ['BC', 'AB', 'SK', 'MB', 'ON', 'QC', 'NB', 'PE', 'NS', 'NL'];
+
+let title = {
+  Canada: "Federal polling",
+  Canada_BC: "British Columbia federal polling",
+  Canada_AB: "Alberta federal polling",
+  Canada_SKMB: "Sask./Man. federal polling",
+  Canada_ON: "Ontario federal polling",
+  Canada_QC: "Quebec federal polling",
+  Canada_ATL: "Atlantic federal polling",
+  BC: "British Columbia provincial polling",
+  AB: "Alberta provincial polling",
+  SK: "Saskatchewan provincial polling",
+  MB: "Manitoba provincial polling",
+  ON: "Ontario provincial polling",
+  QC: "Quebec provincial polling",
+  NB: "New Brunswick provincial polling",
+  PE: "Prince Edward Island provincial polling",
+  NS: "Nova Scotia provincial polling",
+  NL: "Nfld. & Lab. provincial polling",
+  YT: "Yukon territorial polling",
+}
+
 export default function SingleProvince({pageContext, data}) {
     const {jurisdiction, jurisdictionName, beforeDate} = pageContext;
     const polls = data.allPollsJson.nodes;
     const election = data.prevElection;
     const pageTitle = `${jurisdictionName} ${jurisdiction.split("_")[0] !== "Canada" ? (jurisdiction === "YT" ? "territorial" : "provincial") : ""} polling`;
+
+    const lastProvince = jurisdiction === 'BC' ? 'NL' : provinceOrder[provinceOrder.findIndex(x => x === jurisdiction) - 1];
+    const nextProvince = jurisdiction === 'NL' ? 'BC' : provinceOrder[provinceOrder.findIndex(x => x === jurisdiction) + 1];
+
     return(
         <Layout>
         <Seo title={pageTitle} description={`Database of polls conducted since the ${new Date(election.date).getFullYear()} ${jurisdiction.split("_")[0] === "Canada" ? "federal" : jurisdictionName + " " + (jurisdiction === "YT" ? "territorial" : "provincial")} election.`} />
             <h1>{pageTitle}</h1>
             <p>This page lists every publicly accessible poll conducted by a reputable firm after the {new Date(election.date).getFullYear()} {jurisdiction.split("_")[0] === "Canada" ? "federal" : jurisdictionName + " " + (jurisdiction === "YT" ? "territorial" : "provincial")} election. Many polls are privately commissioned and go unreleased, but some are leaked after the fact. To learn more about Polling Canada's standards, check the <Link to="/faq">FAQ</Link>.</p>
             <PollingContent polls={polls} jurisdiction={jurisdiction} name={jurisdictionName} election={election} nextElection={data.nextElection} />
+            <Link to={"/" + lastProvince} className="navArrow leftArrow">{title[lastProvince]}</Link>
+            <Link to={"/" + nextProvince} className="navArrow rightArrow">{title[nextProvince]}</Link>
         </Layout>
     )
 }
