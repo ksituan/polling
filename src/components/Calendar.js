3195aecd4f361d549226107a16c32c77012cdcb3
Alex McPhee <awmcphee@ualberta.ca>
2022-08-04 13:41:50 -0600

    Add flags to election calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,6 +3,40 @@ import * as React from "react"
 import events from "../../content/events.json"
 import parties from "../../content/parties.json"
 
+import BC from '../images/flags/BC.png'
+import AB from '../images/flags/AB.png'
+import SK from '../images/flags/SK.png'
+import MB from '../images/flags/MB.png'
+import ON from '../images/flags/ON.png'
+import QC from '../images/flags/QC.png'
+import NB from '../images/flags/NB.png'
+import PE from '../images/flags/PE.png'
+import NS from '../images/flags/NS.png'
+import NL from '../images/flags/NL.png'
+import NU from '../images/flags/NU.png'
+import NT from '../images/flags/NT.png'
+import YT from '../images/flags/YT.png'
+import Canada from '../images/flags/Canada.png'
+
+// I understand this is terrible code but I don't care right now
+
+const flags = {
+    BC: BC,
+    AB: AB,
+    SK: SK,
+    MB: MB,
+    ON: ON,
+    QC: QC,
+    NB: NB,
+    PE: PE,
+    NS: NS,
+    NL: NL,
+    NU: NU,
+    NT: NT,
+    YT: YT,
+    Canada: Canada
+}
+
 const jurisdictions = {
     BC: "British Columbia",
     AB: "Alberta",
@@ -34,7 +68,10 @@ function Calendar() {
         <div className="calendar">
         {fevents.map((event, n) =>
             <div className={n < 10 ? "event" : "event hidden" }>
-                <div className="desc">{jurisdictions[event.jurisdiction]}</div>
+                <div className="jurisdiction">
+                    <img className="flag" src={flags[event.jurisdiction]} alt="" width="25" />
+                    <div className="desc">{jurisdictions[event.jurisdiction]}</div>
+                </div>
                 <div className="desc">{event.type === "leadership" ? parties.content[event.jurisdiction][event.party].fullName + " leadership election" : titles[event.type]}</div>
                 <div className="date">{event.date.toLocaleDateString('en-CA', {year: "numeric", month: "long", day: "numeric"})}</div>
                 <div className="date">{event.date - new Date() > 24*60*60*1000 ? Math.ceil((event.date - new Date())/(24*60*60*1000)) + " days" : "Tomorrow"}</div>
