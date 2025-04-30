bd2d2529eb44b2e4a0c88807dd96c18742c0e959
Alex <alex@awmcphee.ca>
2025-04-29 22:32:24 -0600

    Emergency cat fix

--- src/components/DashCanada.js
+++ src/components/DashCanada.js
@@ -3,23 +3,23 @@ import DashChart from "./DashChart.js"
 import SinglePollingAverage from "../components/SinglePollingAverage.js"
 import MarkLastPolls from "../components/MarkLastPolls"
 import parties from "../../content/parties.json"
-import {Link} from "gatsby"
+import { Link } from "gatsby"
 
 let brandColours = {
-    "maroon" : "#8b4943",
-    "red" : "#c94141",
-    "orange" : "#e69f4a",
-    "yellow" : "#ebc53f",
-    "green" : "#85ab53",
-    "darkGreen" : "#5f7b3b",
-    "teal" : "#88c9c3",
-    "blue" : "#598dab",
-    "darkBlue" : "#52709e",
-    "purple" : "#af7ac8",
-    "gray" : "#ddd",
+    "maroon": "#8b4943",
+    "red": "#c94141",
+    "orange": "#e69f4a",
+    "yellow": "#ebc53f",
+    "green": "#85ab53",
+    "darkGreen": "#5f7b3b",
+    "teal": "#88c9c3",
+    "blue": "#598dab",
+    "darkBlue": "#52709e",
+    "purple": "#af7ac8",
+    "gray": "#ddd",
 }
 
-function DashCanada({polls, election}) {
+function DashCanada({ polls, election }) {
 
     const relevantParties = parties.content["Canada"];
     let todaysAverage = SinglePollingAverage(MarkLastPolls(polls), new Date(), relevantParties);
@@ -27,22 +27,27 @@ function DashCanada({polls, election}) {
     let majorAverage = todaysAverage //.filter(party => party.score >= 5);
 
     return (
-        <Link className="dashCanada" to={`/Canada-${election.year}`} style={{textDecoration: "none"}}>
+        <Link className="dashCanada" to={`/Canada-${election.year}`} style={{ textDecoration: "none" }}>
             <DashChart polls={polls} jurisdiction="Canada" election={election} plotWidth={1000} plotHeight={400} className="wideChart" />
             <div className="fedPartyContainer">
-            {majorAverage.map((event, n) =>
-                <div className="dashFedParty" key={n}>
-                    <svg className="partyLogo" viewBox="0 0 100 100" style={{width: "1.5rem", height: "1.5rem", padding: "0.2rem", border: `2px solid ${brandColours[relevantParties[event.party].colour]}`, borderRadius: "0.25rem"}}><path fill={brandColours[relevantParties[event.party].colour]} d={relevantParties[event.party].logo} /></svg>
-                    <div>
-                        <div className="dashFedName" style={{color: brandColours[relevantParties[event.party].colour]}}>{event.party}</div>
-                        <div className="dashFedScore" style={{color: event.score > (election.results.filter(x => x.party === event.party)[0]?.score || 0) ? brandColours["green"] : brandColours["red"]}}>
-                            {event.score > (election.results.filter(x => x.party === event.party)[0]?.score || 0) ? "▲" : "▼"}
-                            {event.score % 1 === 0 ? String(event.score) + ".0" : event.score}%
+                {majorAverage.length > 0 ? majorAverage.map((event, n) =>
+                    <div className="dashFedParty" key={n}>
+                        <svg className="partyLogo" viewBox="0 0 100 100" style={{ width: "1.5rem", height: "1.5rem", padding: "0.2rem", border: `2px solid ${brandColours[relevantParties[event.party].colour]}`, borderRadius: "0.25rem" }}><path fill={brandColours[relevantParties[event.party].colour]} d={relevantParties[event.party].logo} /></svg>
+                        <div>
+                            <div className="dashFedName" style={{ color: brandColours[relevantParties[event.party].colour] }}>{event.party}</div>
+                            <div className="dashFedScore" style={{ color: event.score > (election.results.filter(x => x.party === event.party)[0]?.score || 0) ? brandColours["green"] : brandColours["red"] }}>
+                                {event.score > (election.results.filter(x => x.party === event.party)[0]?.score || 0) ? "▲" : "▼"}
+                                {event.score % 1 === 0 ? String(event.score) + ".0" : event.score}%
+                            </div>
                         </div>
                     </div>
-                </div>
-            )}
-            </div> 
+                ) :
+                    <div style={{ gridColumn: "span 6" }}>
+                        <div className="dashFedName" style={{ color: "#B0B0B0" }}>Gone fishing</div>
+                        <div className="dashFedScore" style={{ color: "#B0B0B0" }}>No new polls yet</div>
+                    </div>
+                }
+            </div>
             {/*<div className="dashTitle">House of Commons</div>*/}
         </Link>
     )
